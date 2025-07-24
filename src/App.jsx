0f1e1b6c05f038c11cf2f3b99bdab2a460fbda43
Houston Braga <houstonliverboy123@gmail.com>
2025-07-24 20:07:13 -0300

    chore: add prettier-plugin-tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Welcome to the Priorix</h1>
+  return (
+    <h1 className="mr-3 p-4 font-semibold text-red-700">
+      Welcome to the Priorix
+    </h1>
+  )
 }
 
 export default App
