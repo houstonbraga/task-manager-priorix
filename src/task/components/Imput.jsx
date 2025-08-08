f84f9ee5585817e629a2df66b1f2e134c2a863c7
Houston Braga <houstonliverboy123@gmail.com>
2025-08-08 19:35:37 -0300

    feat: add InputLabel component

--- src/task/components/Imput.jsx
+++ src/task/components/Imput.jsx
@@ -1,9 +1,9 @@
+import InputLabel from "./InputLabel"
+
 const Input = ({ label, ...rest }) => {
   return (
     <div className="flex flex-col gap-1 text-left">
-      <label className="text-sm text-zinc-400" htmlFor={rest.id}>
-        {label}
-      </label>
+      <InputLabel label={label} />
       <input
         className="rounded-md border border-solid border-zinc-500 bg-transparent px-4 py-2"
         {...rest}
