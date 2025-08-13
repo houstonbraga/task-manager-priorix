f9b25f31bcf93312a6284c45cde899675fdd6e63
Houston Braga <houstonliverboy123@gmail.com>
2025-08-13 16:13:56 -0300

    chore: add prop-types to InputLabel

--- src/task/components/InputLabel.jsx
+++ src/task/components/InputLabel.jsx
@@ -1,9 +1,15 @@
+import PropTypes from "prop-types"
+
 const InputLabel = ({ label, ...rest }) => {
   return (
     <label className="text-sm text-zinc-400" htmlFor={rest.id}>
       {label}
     </label>
   )
+}
+
+InputLabel.propTypes = {
+  label: PropTypes.string.isRequired,
 }
 
 export default InputLabel
