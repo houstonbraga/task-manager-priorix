eb4cab790bad3ed6d5fc511985cb9655243aa947
Houston Braga <houstonliverboy123@gmail.com>
2025-08-13 16:12:04 -0300

    chore: add prop-types to InputErrorMessage

--- src/task/components/InputErrorMessage.jsx
+++ src/task/components/InputErrorMessage.jsx
@@ -1,5 +1,11 @@
+import PropTypes from "prop-types"
+
 const InputErrorMessage = ({ children }) => {
   return <span className="text-xs text-red-500">{children}</span>
+}
+
+InputErrorMessage.propTypes = {
+  children: PropTypes.string,
 }
 
 export default InputErrorMessage
