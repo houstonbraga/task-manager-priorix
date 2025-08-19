326cb60b8e73ce62ed679e3034a7c62163d9b384
Houston Braga <houstonliverboy123@gmail.com>
2025-08-18 23:57:24 -0300

    style: edit color's TaskDialog component

--- src/task/components/AddTaskDialog.jsx
+++ src/task/components/AddTaskDialog.jsx
@@ -76,7 +76,7 @@ const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
         ref={nodeRef}
         className="fixed bottom-0 left-0 right-0 top-0 flex h-screen w-screen flex-col items-center justify-center backdrop-blur-sm"
       >
-        <div className="flex w-[366px] flex-col space-y-4 rounded-xl bg-zinc-800 p-5 text-center text-white">
+        <div className="flex w-[366px] flex-col space-y-4 rounded-xl bg-zinc-700 p-5 text-center text-white">
           <div>
             <h2 className="mb-2 text-xl font-semibold">Nova tarefa</h2>
             <p className="text-sm text-zinc-500">
