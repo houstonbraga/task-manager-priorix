8a8f82b4fe87fe5406d04d99bef5ab8a7c52f236
Houston Braga <houstonliverboy123@gmail.com>
2025-08-22 14:53:21 -0300

    chore: add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,24 @@ import "./index.css"
 
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 import App from "./App.jsx"
+import TaskDetails from "./pages/task-details.jsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/tasks/:taskId",
+    element: <TaskDetails />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
---
+++ src/pages/task-details.jsx
@@ -0,0 +1,5 @@
+const TaskDetails = () => {
+  return <div>Ola mundo</div>
+}
+
+export default TaskDetails
